cd9ed414755b3487304ff91eb84f4efc06da1f62
Aurélien Spinelli <aurelien.spinelli@hotmail.fr>
2018-10-24 16:42:42 +0200

    add method

--- services/announcement/src/main/java/fr/unice/polytech/al/repository/AnnouncementRepository.java
+++ services/announcement/src/main/java/fr/unice/polytech/al/repository/AnnouncementRepository.java
@@ -8,4 +8,5 @@ import org.springframework.data.repository.CrudRepository;
 public interface AnnouncementRepository extends CrudRepository<Announcement, Long> {
 
     List<Announcement> findByStartPoint(String startPoint);
+    List<Announcement> findByEndPoint(String endPoint);
 }
