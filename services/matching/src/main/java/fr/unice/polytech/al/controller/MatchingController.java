bfaa92e7ef30971258ea414cf41a5f987b511797
andre <andreina_844@hotmail.com>
2018-10-24 18:42:03 +0200

    matching simple mock

--- services/matching/src/main/java/fr/unice/polytech/al/controller/MatchingController.java
+++ services/matching/src/main/java/fr/unice/polytech/al/controller/MatchingController.java
@@ -1,5 +1,8 @@
 package fr.unice.polytech.al.controller;
 
+import fr.unice.polytech.al.model.Course;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MatchingController
 {
+    /*
+    * Input: Id of the new announcement
+    * Output: json object with an array of the json representation of the courses
+    */
     @GetMapping("/match/{idAnnonce}")
-    public String match(@PathVariable String idAnnonce) {
-        return idAnnonce;
+    public String match(@PathVariable long idAnnonce)
+    {
+        JSONArray coursesMocked = new JSONArray();
+
+        coursesMocked.put((new Course(idAnnonce)).toJson());
+        coursesMocked.put((new Course(2)).toJson());
+        coursesMocked.put((new Course(3)).toJson());
+
+       return new JSONObject().put("courses", coursesMocked).toString();
     }
 }
--- services/matching/src/main/java/fr/unice/polytech/al/model/Course.java
+++ services/matching/src/main/java/fr/unice/polytech/al/model/Course.java
@@ -1,10 +1,35 @@
 package fr.unice.polytech.al.model;
 
+import org.json.JSONObject;
+import java.util.Date;
+
 public class Course {
 
     private Long id;
+    private String idClient;
+    private String idDriver;
+    private String idAnnouncement;
+    private String startPoint;
+    private String endPoint;
+    private Date startDate;
+    private Date endDate;
 
-    public Course() {
+    public Course(long id) {
+        this.id = id;
+    }
 
+    public Course(String idClient, String idDriver, String idAnnouncement, String startPoint, String endPoint, Date startDate, Date endDate){
+        this.idClient = idClient;
+        this.idDriver = idDriver;
+        this.idAnnouncement = idAnnouncement;
+        this.startPoint = startPoint;
+        this.endPoint = endPoint;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public JSONObject toJson() {
+        return new JSONObject()
+                .put("id", id);
     }
 }
