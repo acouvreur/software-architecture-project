17d2ba78e40f7536301cca063c1adb7d661ef4de
Alexis Couvreur <alexiscouvreur.pro@gmail.com>
2018-10-26 11:36:36 +0200

    WIP, needs tests

--- services/announcement/src/main/java/fr/unice/polytech/al/assembler/AnnouncementResourceAssembler.java
+++ services/announcement/src/main/java/fr/unice/polytech/al/assembler/AnnouncementResourceAssembler.java
@@ -1,4 +1,15 @@
 package fr.unice.polytech.al.assembler;
 
-public class AnnouncementResourceAssembler {
+import fr.unice.polytech.al.controller.AnnouncementController;
+import fr.unice.polytech.al.model.Announcement;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.ResourceAssembler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnnouncementResourceAssembler implements ResourceAssembler<Announcement, Resource<Announcement>> {
+    @Override
+    public Resource<Announcement> toResource(Announcement announce) {
+        return new Resource<>(announce);
+    }
 }
--- services/announcement/src/main/java/fr/unice/polytech/al/repository/AnnouncementRepository.java
+++ services/announcement/src/main/java/fr/unice/polytech/al/repository/AnnouncementRepository.java
@@ -3,9 +3,10 @@ package fr.unice.polytech.al.repository;
 import java.util.List;
 
 import fr.unice.polytech.al.model.Announcement;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface AnnouncementRepository extends CrudRepository<Announcement, Long> {
+public interface AnnouncementRepository extends JpaRepository<Announcement, Long> {
 
     List<Announcement> findByStartPoint(String startPoint);
     List<Announcement> findByEndPoint(String endPoint);
