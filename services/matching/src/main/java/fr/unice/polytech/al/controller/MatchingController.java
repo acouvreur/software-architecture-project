672372acbc99806862570c57bd1166d027297233
andre <andreina_844@hotmail.com>
2018-11-07 11:07:49 +0100

    matching

--- services/matching/src/main/java/fr/unice/polytech/al/controller/MatchingController.java
+++ services/matching/src/main/java/fr/unice/polytech/al/controller/MatchingController.java
@@ -1,8 +1,9 @@
 package fr.unice.polytech.al.controller;
 
-import fr.unice.polytech.al.model.Announcement;
-import org.json.JSONArray;
+import fr.unice.polytech.al.kafka.MatchingKafkaSender;
+import fr.unice.polytech.al.model.Match;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,19 +11,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MatchingController
 {
+    @Autowired
+    MatchingKafkaSender kafkaSender;
+
+    private Match matchList = new Match();
+
     /*
-    * Input: Id of the new announcement
-    * Output: json object with an array of the json representation of the courses
+    * Input: Id of the announcement created
+    * Output: List of matching announcement
     */
     @GetMapping("/match/{idAnnonce}")
-    public String match(@PathVariable long idAnnonce)
+    public String match(@PathVariable String idAnnonce)
     {
-        JSONArray coursesMocked = new JSONArray();
+        //kafkaSender.send("announcement_created", (new JSONObject().put("announcementId", 10)).toString());
 
-        coursesMocked.put((new Announcement(idAnnonce)).toJson());
-        coursesMocked.put((new Announcement(2)).toJson());
-        coursesMocked.put((new Announcement(3)).toJson());
+        matchList.add(idAnnonce);
+        String result = matchList.toJson().toString();
 
-       return new JSONObject().put("announcements", coursesMocked).toString();
+      //  if (matchList.size() > 1) {
+           // kafkaSender.send("create_announcement", result);
+      //  }
+
+        if (matchList.size() == 3) {
+            matchList.empty();
+        }
+
+        return result;
     }
 }
