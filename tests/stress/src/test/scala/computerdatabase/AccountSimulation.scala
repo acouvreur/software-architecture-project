f77f14190332b4b563149c4104eb9efedcaf6429
Aurélien Spinelli <aurelien.spinelli@hotmail.fr>
2018-11-08 18:59:06 +0100

    stress test

--- tests/stress/src/test/scala/computerdatabase/AccountSimulation.scala
+++ tests/stress/src/test/scala/computerdatabase/AccountSimulation.scala
@@ -4,51 +4,44 @@ import java.util.UUID
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
+import org.apache.commons.lang.RandomStringUtils
 
 import scala.concurrent.duration._
 import scala.language.postfixOps
 
-class CourseSimulation extends Simulation{
-
+class AccountSimulation extends Simulation{
   val httpProtocol = http
-    .baseUrl("http://localhost:8083/")
+    .baseUrl("http://localhost:8081/")
     .acceptHeader("application/json")
     .header("Content-Type", "application/json")
 
   val stressSample =
-    scenario("Course")
+    scenario("Account")
       .repeat(2)
       {
         exec(session =>
-          session.set("id", UUID.randomUUID().toString)
+          session.set("mail", RandomStringUtils.randomAlphabetic(50))
+            .set("name", RandomStringUtils.randomAlphabetic(50))
         )
           .exec(
-            http("Create_Course")
-              .post("courses")
-              .body(StringBody(session => buildCourse(session)))
+            http("Create_Account")
+              .post("accounts")
+              .body(StringBody(session => buildAccount(session)))
               .check(status.is(201))
           )
           .pause(1 seconds)
           .exec(
-            http("Consult_Course")
-              .get("courses?announcement=42")
+            http("Consult_Account")
+              .get("accounts/AlexLeBoss")
               .check(status.is(200))
           )
           .pause(1 seconds)
       }
 
-  def buildCourse(session: Session): String = {
-    val id = session("id").as[String]
-    raw"""{
-         "idClient":456,
-         "idDriver":2345,
-         "idAnnouncement":42,
-         "idNextCourse":3,
-         "startPoint":"Paris",
-         "endPoint":"Nice",
-         "startDate":"2018-11-01",
-         "endDate":"2018-11-02"
-    }""""
+  def buildAccount(session: Session): String = {
+    val mail = session("mail").as[String]
+    val name = session("name").as[String]
+    raw"""{"email":"$mail", "username":"$name", "firstName":"Alexis", "lastName":"Couvreur"}""""
   }
 
   setUp(stressSample.inject(constantConcurrentUsers(100) during (10 seconds), // 1
