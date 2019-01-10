f50fdf4e1606ddc5c29d38dd88e017f331d8bc79
Aurélien Spinelli <aurelien.spinelli@hotmail.fr>
2019-01-10 14:53:38 +0100

    setup serializer and deserializer

---
+++ services/announcement/src/main/java/fr/unice/polytech/al/deserializer/AnnouncementDeserializer.java
@@ -0,0 +1,4 @@
+package fr.unice.polytech.al.deserializer;
+
+public class AnnouncementDeserializer {
+}
---
+++ services/announcement/src/main/java/fr/unice/polytech/al/serializer/AnnouncementSerializer.java
@@ -0,0 +1,4 @@
+package fr.unice.polytech.al.serializer;
+
+public class AnnouncementSerializer {
+}
