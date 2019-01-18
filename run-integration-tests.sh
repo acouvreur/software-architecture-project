d61825e20b20f8f9078677c9b7bbdbbb72fb04f8
Alexis Couvreur <alexiscouvreur.pro@gmail.com>
2019-01-18 10:24:12 +0100

    travis update

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ## Run integration tests
-docker run --rm --network host -it -v "$(pwd)"/tests/integration:/project maven:3.5.2-jdk-8-alpine mvn integration-test -pl tests/acceptation -f /project
+# docker run --rm --network host -it -v "$(pwd)"/tests/integration:/project maven:3.5.2-jdk-8-alpine mvn integration-test -pl tests/acceptation -f /project
 
 ## Run scenario
 docker run --rm --network host integration
--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 ## Run all services unit tests
-docker run --rm --network host -it -v "$(pwd)":/project maven:3.5.2-jdk-8-alpine mvn test -pl services -f /project
+# docker run --rm --network host -it -v "$(pwd)":/project maven:3.5.2-jdk-8-alpine mvn test -pl services -f /project
+docker run --rm -it -v "$(pwd)":/project maven:3.5.2-jdk-8-alpine mvn clean install -f /project
 
