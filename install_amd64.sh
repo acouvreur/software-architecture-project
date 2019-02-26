6c8b70b6b65dee3b598baa35ca4093ad25f9cb62
Alexis Couvreur <alexiscouvreur.pro@gmail.com>
2019-02-26 20:40:39 +0100

    Automatic push to the registry

--- install_amd64.sh
+++ install_amd64.sh
@@ -2,3 +2,9 @@
 
 mvn clean package -DskipTests=true
 docker-compose -f ./docker-compose-linux-amd64.yaml build
+
+docker push localhost:5000/al-blablamove-announcement:amd64
+docker push localhost:5000/al-blablamove-accounting:amd64
+docker push localhost:5000/al-blablamove-billing:amd64
+docker push localhost:5000/al-blablamove-matching:amd64
+docker push localhost:5000/al-blablamove-tracking:amd64
