81c28ca87a994097cc1d65cbf093a34af63f1518
Aurélien Spinelli <aurelien.spinelli@hotmail.fr>
2019-02-27 19:48:23 +0100

    fix

--- setup_and_run.sh
+++ setup_and_run.sh
@@ -4,15 +4,13 @@
 docker swarm init
 
 # 2. Create a local registry
-sudo docker service create -p 5000:5000 --name registry \                                                                                           37.29   ✔  19:08:54 | 27.02.19  
-  --mount type=volume,source=my-registry,destination=/var/lib/registry --constraint 'node.role==manager' \
-  registry:2
+sudo docker service create -p 5000:5000 --name registry --mount type=volume,source=my-registry,destination=/var/lib/registry --constraint 'node.role==manager' registry:2
 
 # 5. Create kafka-net network
 docker network create --driver overlay kafka-net
 
 # 3. Install, Tag and Push blablamove services
-install.sh
+./install.sh
 
 # 4. Start the visualizer
 docker run -it -d -p 5050:8080 -v /var/run/docker.sock:/var/run/docker.sock dockersamples/visualizer
